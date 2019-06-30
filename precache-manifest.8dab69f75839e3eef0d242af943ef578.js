3021b361d1f53c84d15ab039368482cff7cecfc4
himanish58 <himanish.kabiraj@gmail.com>
2019-07-01 02:43:00 +0530

    Updates

---
+++ precache-manifest.8dab69f75839e3eef0d242af943ef578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deffb75633163d4c916f1e6078b3e267",
+    "url": "/small-react-app/index.html"
+  },
+  {
+    "revision": "0e68a5d026bcc2226915",
+    "url": "/small-react-app/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "cef22a854855a9e7c10c",
+    "url": "/small-react-app/static/css/main.bbb24b61.chunk.css"
+  },
+  {
+    "revision": "0e68a5d026bcc2226915",
+    "url": "/small-react-app/static/js/2.f15307fe.chunk.js"
+  },
+  {
+    "revision": "cef22a854855a9e7c10c",
+    "url": "/small-react-app/static/js/main.d0dde5e3.chunk.js"
+  },
+  {
+    "revision": "3ff25b88dd299d5c40e2",
+    "url": "/small-react-app/static/js/runtime~main.2bbd02d3.js"
+  }
+]);
